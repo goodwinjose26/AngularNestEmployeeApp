6b669b6f5f4cff9aaf6d0dca96012ec364dd3360
goodwinjose26 <josegoodwin22@gmail.com>
2022-12-11 22:15:00 +0530

    view

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -14,9 +14,7 @@ export class ApiService {
   {
     return this.http.post("http://localhost:8080/addemployee",datatosend)
   }
-  // loginUser = (data:any)=>{
-  //   return this.http.post("http://localhost:8080/login", data)
-  // }
+  
    addLogin=(dataToSend:any)=>
    {
      return this.http.post("http://localhost:8080/login",dataToSend)

044c40bd221c1d71781b1abcd8c73230c067690e
goodwinjose26 <josegoodwin22@gmail.com>
2022-12-11 18:07:33 +0530

    admin login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminnavbarComponent } from './adminnavbar/adminnavbar.component';
+import { AdminloginComponent } from './adminlogin/adminlogin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdminnavbarComponent
+    AdminnavbarComponent,
+    AdminloginComponent
   ],
   imports: [
     BrowserModule,
